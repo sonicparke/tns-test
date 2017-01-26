c0003d77c0408d4f72fd0bd7eb89f43720be684a
Nathan Walker <walkerrunpdx@gmail.com>
2017-01-26 15:58:24 -0800

    fix: component setup (#1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@wwwalkerrun/nativescript-ngx-magic';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-root',
-  templateUrl: './app/app.component.html',
-  styleUrls: ['./app/app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
